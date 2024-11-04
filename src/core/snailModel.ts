99fdd2bcb5fbef49c562883a0699ac8ed65c07e1
mc.lee <289959141@qq.com>
2024-11-04 23:48:17 +0800

    :bug: fix(src/core/snailModel.ts): 重构思路

--- src/core/snailModel.ts
+++ src/core/snailModel.ts
@@ -59,3 +59,52 @@ export const createEntityForClass = <TClass = object>(target: TClass) => {
   (target as SnailModel<TClass & IJson>).fromJson = loadEntity;
   return target as SnailModel<TClass & IJson>;
 };
+
+type TransformOptionsByOneKey<T = IJson> = {
+  [key in keyof T]?:
+    | {
+        alias?: string;
+        required?: boolean;
+        default?: T[key];
+      }
+    | {
+        prefix?: string;
+        required?: boolean;
+        default?: T[key];
+      };
+};
+
+type TransformOptions<T = IJson> =
+  | TransformOptionsByOneKey<T>
+  | {
+      prefix: string;
+    };
+
+export const transformDTO = <T extends IJson>(
+  data: T,
+  transformOptions?: TransformOptions
+): T => {
+  let obj: IJson = {};
+  if (transformOptions === undefined) {
+    return Object.assign({}, data);
+  } else {
+    if (transformOptions.prefix) {
+      for (let key in data) {
+        // 提取前缀和key
+        const objKey = key.replace(transformOptions.prefix as string, "");
+        obj[objKey] = data[key];
+      }
+    } else {
+      for (let key in data) {
+        const value = data[key];
+        // 查找别名
+        const alias = Object.keys(transformOptions).find(
+          (transformKey) => (transformOptions as TransformOptionsByOneKey<T>)[transformKey]?.alias === key
+        );
+        if (alias) {
+          obj[transformOptions[alias]] = value;
+        }
+      }
+    }
+  }
+};
