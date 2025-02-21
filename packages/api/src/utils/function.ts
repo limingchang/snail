dc1e2abc53e5a96c0ba80c32771dfc4512db2c0f
mc.lee <289959141@qq.com>
2025-02-21 11:37:08 +0800

    :sparkles: feat(packages/api/package.json,packages/api/src/utils/function.ts): 优化key的生成，短hash

--- packages/api/src/utils/function.ts
+++ packages/api/src/utils/function.ts
@@ -11,18 +11,31 @@ export const apiKey = <
   apiInstance: T
 ) => {
   const { version, method, url, params, headers } = apiInstance;
-  return `[${method}]${version ? `-v${version}` : ""}-${url}${
-    params ? `-${recordToString(params)}` : ""
-  }${headers ? `-${recordToString(headers)}` : ""}`;
+  const str = `${params ? `-${recordToString(params)}` : ""}${
+    headers ? `-${recordToString(headers)}` : ""
+  }`;
+  return `[${method}]${version ? `-v${version}` : ""}-${url}-${generateShortUniqueHash(str)}`;
 };
 
-// export function createPromise(resolveResult: any, rejectResult: any) {
-//   const promise = new Promise((resolve, rejectResult) => {
-//     resolve(resolveResult);
-//     rejectResult(rejectResult);
-//   });
-//   return promise
-// }
+export async function generateShortUniqueHash(str: string) {
+  // 创建一个文本编码器实例
+  const encoder = new TextEncoder();
+  const data = encoder.encode(str);
+
+  // 使用SHA-256算法生成哈希值
+  const hashBuffer = await window.crypto.subtle.digest("SHA-256", data);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  let hashBase64 = btoa(String.fromCharCode.apply(null, hashArray));
+
+  // 替换Base64编码中的特殊字符
+  hashBase64 = hashBase64
+    .replace(/\+/g, "0")
+    .replace(/\//g, "1")
+    .replace(/=/g, "");
+
+  // 截取前16个字符作为短哈希值
+  return hashBase64.substring(0, 16);
+}
 
 export function recordToString(record: Record<string, any>) {
   return Object.keys(record)
@@ -32,4 +45,3 @@ export function recordToString(record: Record<string, any>) {
     .sort((a, b) => a.localeCompare(b))
     .join("&");
 }
-
